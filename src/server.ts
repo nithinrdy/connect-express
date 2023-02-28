f043c6f771574e125a61b2733515af827d137c48
Nithin <78612244+nithinrdy@users.noreply.github.com>
2023-03-01 00:11:55 +0530

    no longer using it behind gin; set up as a socketio-only server

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,68 @@
-import express from "express";
-import cors from "cors";
-import { corsOptions, setCORSHeaders } from "../config/corsSettings";
-import ProxyRouter from "../routes/proxyRoute";
+import { Server } from "socket.io";
 
-const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors(corsOptions));
-app.use(setCORSHeaders);
+const io = new Server();
 
-app.use("/express-proxy-api", ProxyRouter)
+io.on("connection", (socket) => {
+	const {
+		id,
+		handshake: {
+			query: { username },
+		},
+	} = socket;
+	console.log("User " + username + " connected with socketID " + id);
+	socket.join(username as string);
 
-app.listen(port, () => {
-	console.log(`Listening at http://localhost:${port}`);
+	socket.on("sendOffer", (data) => {
+		const { callee, caller, sdp, type } = data;
+		io.sockets.in(callee).emit("incomingOffer", { callee, caller, sdp, type });
+	});
+
+	socket.on("sendCandidate", (data) => {
+		const userToSendTo = data.to;
+		const candidate = data.candidate;
+		io.sockets.in(userToSendTo).emit("incomingCandidate", candidate);
+	});
+
+	socket.on("sendAnswer", (data) => {
+		const caller = data.to;
+		const sdp = data.sdp;
+		const type = data.type;
+		io.sockets.in(caller).emit("incomingAnswer", { sdp, type });
+	});
+
+	socket.on("endOtherEnd", (data) => {
+		io.sockets.in(data.otherEnd).emit("endCall");
+	});
+
+	socket.on("startCall", (data) => {
+		if (!io.sockets.adapter.rooms.get(data.otherEnd)) {
+			io.sockets.in(data.caller).emit("notOnline", {
+				message: "User " + data.caller + " is not online",
+			});
+			return;
+		}
+		io.sockets.in(data.otherEnd).emit("incomingCall", { caller: data.caller });
+	});
+
+	socket.on("rejectCall", (data) => {
+		io.sockets.in(data.caller).emit("callRejected");
+	});
+
+	socket.on("acceptCall", (data) => {
+		io.sockets.in(data.caller).emit("callAccepted");
+	});
+
+	socket.on("disconnect", () => {
+		console.log("User " + username + " disconnected with socketID " + id);
+		socket.leave(username as string);
+	});
+});
+
+io.listen(port as number, {
+	cors: {
+		origin: "*",
+		methods: ["GET", "POST"],
+	},
 });
