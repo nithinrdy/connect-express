0910292f49f46c1f7bb97b644e95c0b9c21da810
Nithin <78612244+nithinrdy@users.noreply.github.com>
2023-03-02 23:24:06 +0530

    why cors, why?

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,7 +2,7 @@ import { CorsOptions } from "cors";
 
 const corsOptions: CorsOptions = {
 	origin: function (origin, callback) {
-		callback(null, true); // Allow all origins since server operates behind a reverse proxy
+		callback(null, true); // Still figuring this out...
 	},
 };
 
